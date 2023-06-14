620ef49c863f0841f921eff73b86448e370af865
GiselleAnido <giselle.anido.zapata@gmail.com>
2023-06-14 11:49:54 +0200

    c

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:  "https://rescuemedemofinal.vercel.app",
+    origin: "https://rescuemedemofinal.vercel.app",
     credentials: true,
   })
 );
